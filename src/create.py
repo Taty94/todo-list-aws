b964c74e155afa660482baee12621f54c3c5669e
Taty94 <92807630+Taty94@users.noreply.github.com>
2025-06-16 16:55:25 -0500

    Update create.py
    Revertir warnings

--- src/create.py
+++ src/create.py
@@ -1,15 +1,12 @@
 import json
 import logging
 import todoList
-import sys  # Warning: 'sys' imported but unused
 
 def create(event, context):
     data = json.loads(event['body'])
     if 'text' not in data:
         logging.error("Validation failed")
         raise Exception("Couldn't create the todo item.")
-    # Warning para Bandit: uso inseguro de eval
-    result = eval("2 + 2")
     item = todoList.put_item(data['text'])
     # create a response
     response = {
