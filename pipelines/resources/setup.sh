e84718c106d59a0f803ac9446438cf7ba55d5789
Tatiana Montenegro <tmontene@pichincha.com>
2025-06-24 14:03:13 -0500

    Add resources folder

---
+++ pipelines/resources/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+python -m venv todo-list-aws
+source todo-list-aws/bin/activate
+python -m pip install --upgrade pip
+#For static testing
+python -m pip install flake8
+python -m pip install flake8-polyfill
+python -m pip install bandit
+#For integration testing
+python -m pip install pytest
+
+
+pwd
