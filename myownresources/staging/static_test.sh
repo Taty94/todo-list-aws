387becf0d418802f723b7d88c37fade226bbf904
Tatiana Montenegro <tmontene@pichincha.com>
2025-06-24 16:27:07 -0500

    Modify static_test script

--- myownresources/staging/static_test.sh
+++ myownresources/staging/static_test.sh
@@ -6,10 +6,11 @@ set -x
 flake8 --format=pylint src >flake8.out
 if [[ $? -ne 0 ]]
 then
-    exit 1
+   # exit 1
+   echo "\033[1;33mFlake8 encontró problemas, pero la ejecución continuará.\033[0m"
 fi
 bandit -r src -f custom -o bandit.out --msg-template "{abspath}:{line}: [{test_id}] {msg}"
 if [[ $? -ne 0 ]]
 then
-    exit 1
+    echo "\033[1;33mBandit encontró problemas, pero la ejecución continuará.\033[0m"
 fi
