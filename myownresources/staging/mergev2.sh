b3c4b1a50af880b788a18f2165570b93a25e1489
Tatiana Montenegro <tmontene@pichincha.com>
2025-06-29 17:40:00 -0500

    Add token to push

--- myownresources/staging/mergev2.sh
+++ myownresources/staging/mergev2.sh
@@ -2,8 +2,12 @@
 
 set -x
 
+# Variables
+GITHUB_TOKEN="${GITHUB_TOKEN}"
+
 git branch -a
 git checkout -b develop origin/develop
 git checkout -b master origin/master
 git merge develop
+git remote set-url origin https://${GITHUB_TOKEN}@github.com/Taty94/todo-list-aws.git
 git push origin master 
